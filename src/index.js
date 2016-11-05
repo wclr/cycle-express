7c4ca1bde99b09870e5e6a4da43b11dc9b83e308
Alex0007 <Alex0007lolpvp@gmail.com>
2016-11-05 19:53:08 +0500

    xstream

--- src/index.js
+++ src/index.js
@@ -25,19 +25,19 @@ var terminateMethodsMap = terminateMethods.reduce((methods, m) => {
 const isExpressRouter = router => router && typeof router.use === 'function'
 
 const makeRouterDriver = (routerOptions, options = {}) => {
-  const router = isExpressRouter(routerOptions) ? routerOptions : Router(routerOptions)
+  const requestsStore = {}
 
-  let _requests = {}
+  const router = isExpressRouter(routerOptions) ? routerOptions : Router(routerOptions)
 
   const createDriverRouter = router => {
     const driverRouter = {}
     const createReq$ = (method, path) => {
       const req$ = xs.create({
-        id: cuid(),
         start: listener => {
           router[method](path, (req, res, next) => {
-            req.id = this.id
-            _requests[this.id] = { req, res }
+            const id = cuid()
+            req.id = id
+            requestsStore[id] = { req, res }
             listener.next(req)
           })
         },
@@ -62,34 +62,35 @@ const makeRouterDriver = (routerOptions, options = {}) => {
     return driverRouter
   }
 
-  const handle = response => {
-    var _r = _requests[response.id]
-    if (_r) {
-      let res = _r.res
-      let terminate
-      let methods = []
-      for (let key in response) {
-        if (typeof res[key] === 'function') {
-          if (terminateMethodsMap[key]) {
-            terminate = key
-          } else {
-            methods.push(key)
+  return outgoing$ => {
+    outgoing$.addListener({
+      next: response => {
+        const _r = requestsStore[response.id]
+
+        if (!_r) throw new Error(`request with id ${response.id} not found`)
+
+        let terminate
+        const res = _r.res
+        const methods = []
+        for (let key in response) {
+          if (typeof res[key] === 'function') {
+            if (terminateMethodsMap[key]) {
+              terminate = key
+            } else {
+              methods.push(key)
+            }
           }
         }
+        terminate && methods.push(terminate)
+
+        methods.forEach(m => res[m](response[m]))
+
+        if (terminate) {
+          delete requestsStore[response.id]
+        }
       }
-      terminate && methods.push(terminate)
-      methods.forEach(
-        m => res[m](response[m])
-      )
-      if (terminate) {
-        delete _requests[response.id]
-      }
-    } else {
-      throw new Error(`request with id ${response.id} not found`)
-    }
-  }
-  return response$ => {
-    response$.forEach(handle)
+    })
+
     return createDriverRouter(router)
   }
 }
